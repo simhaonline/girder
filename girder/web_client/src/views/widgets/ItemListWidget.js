cd27883ef2b456db25e7a3c430082115b34cfe63
Bryon Lews <Bryon.Lewis@kitware.com>
2020-04-01 13:57:18 -0400

    small change to fix folder swapping

--- girder/web_client/src/views/widgets/ItemListWidget.js
+++ girder/web_client/src/views/widgets/ItemListWidget.js
@@ -63,6 +63,8 @@ var ItemListWidget = View.extend({
         this.collection = new ItemCollection();
         this.collection.append = true; // Append, don't replace pages
         this.collection.filterFunc = settings.itemFilter;
+        this.currentPage = 1; // By default we want to be on the first page
+
         if (this._paginated) {
             this.collection.append = false;
         }
